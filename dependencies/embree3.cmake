26bc108f9f2044ef70ceb899bed6d07de5c11dae
Teleskopist <ndp1997forever@gmail.com>
2026-06-29 02:16:25 +0300

    fix for my os

--- dependencies/embree3.cmake
+++ dependencies/embree3.cmake
@@ -9,7 +9,16 @@ if(WIN32)
         IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/embree3/bin_win64/embree3.dll
         IMPORTED_IMPLIB ${CMAKE_CURRENT_LIST_DIR}/embree3/lib_win64/embree3.lib)
 else()
-    set_target_properties(embree3 PROPERTIES IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/embree3/lib/libembree3.so)
+    # Some distributions include versioned sonames (libembree3.so.3); prefer that if present
+    if(EXISTS ${CMAKE_CURRENT_LIST_DIR}/embree3/lib/libembree3.so)
+        set(_embree_lib ${CMAKE_CURRENT_LIST_DIR}/embree3/lib/libembree3.so)
+    elseif(EXISTS ${CMAKE_CURRENT_LIST_DIR}/embree3/lib/libembree3.so.3)
+        set(_embree_lib ${CMAKE_CURRENT_LIST_DIR}/embree3/lib/libembree3.so.3)
+    else()
+        set(_embree_lib ${CMAKE_CURRENT_LIST_DIR}/embree3/lib/libembree3.so)
+    endif()
+
+    set_target_properties(embree3 PROPERTIES IMPORTED_LOCATION ${_embree_lib})
 endif()
 
 target_include_directories(embree3 INTERFACE ${CMAKE_CURRENT_LIST_DIR}/embree3)
